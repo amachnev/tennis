ad597960448bc904778a6dc703d254b191768058
Art Machnev <a.machnev@ankuraconsulting.com>
2018-03-27 00:39:14 -0400

    Add a unit testing project

---
+++ tests/Tennis.Web.Tests/FirstTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Tennis.Web.Tests
+{
+    [TestFixture]
+    public class FirstTest
+    {
+        [Test]
+        public void AssertThatProductOfTwoAndTwoEqualsFour()
+        {
+            Assert.That(2*2, Is.EqualTo(4));
+        }
+    }
+}
