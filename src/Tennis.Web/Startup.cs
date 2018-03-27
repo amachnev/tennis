e1e53731edbf54966ac4aa8009d5d52d4aceb69b
Art Machnev <a.machnev@ankuraconsulting.com>
2018-03-26 23:25:26 -0400

    Add solution file, Tennis.Web project and a basic landing page

---
+++ src/Tennis.Web/Startup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Tennis.Web
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseMvc();
+        }
+    }
+}
